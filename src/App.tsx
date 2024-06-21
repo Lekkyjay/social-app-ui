be127ccc5158bf8377e5ed8871dd3f0c528ce8a1
‘Lekky <lekkyjay@yahoo.com>
2024-06-21 16:12:55 +0200

    Register page implemented.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Login from './pages/login/Login'
+import Register from './pages/register/Register'
 
 function App() {
   
+  const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: <Login />      
+    },
+    {
+      path: "/register",
+      element: <Register />      
+    }
+  ])
 
   return (
     <>
-      <Login />
+      <RouterProvider router={router} />
     </>
   )
 }
--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,30 +1,30 @@
 import { Link } from 'react-router-dom'
-import './login.scss'
+import './register.scss'
 
-const handleLogin = () => {}
-
-export default function Login() {
+export default function Register() {
   return (
-    <div className="login">
+    <div className="register">
       <div className="card">
         <div className="left">
-          <h1>Hello World.</h1>
+          <h1>Cheez Media.</h1>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
             alias totam numquam ipsa exercitationem dignissimos, error nam,
             consequatur.
           </p>
-          <span>Don't you have an account?</span>
-          <Link to="/register">
-            <button>Register</button>
+          <span>Do you have an account?</span>
+          <Link to="/login">
+            <button>Login</button>
           </Link>
         </div>
         <div className="right">
-          <h1>Login</h1>
+          <h1>Register</h1>
           <form>
             <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
             <input type="password" placeholder="Password" />
-            <button onClick={handleLogin}>Login</button>
+            <input type="text" placeholder="Name" />
+            <button>Register</button>
           </form>
         </div>
       </div>
