f4c768b25f0517115565f3bcdada59455ea78077
‘Lekky <lekkyjay@yahoo.com>
2024-06-21 17:16:38 +0200

    Layout implemented.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Login from './pages/login/Login'
 import Register from './pages/register/Register'
+import Home from './pages/home/Home'
+import Layout from './components/layout/Layout'
+import Profile from './pages/profile/Profile'
 
 function App() {
   
   const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />      
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />      
+        }
+      ]
+    },
     {
       path: "/login",
       element: <Login />      
---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,20 @@
+import { Outlet } from 'react-router-dom'
+import LeftBar from '../leftBar/LeftBar'
+import Navbar from '../navbar/Navbar'
+import RightBar from '../rightBar/RightBar'
+import './layout.scss'
+
+export default function Layout() {
+  return (
+    <div>
+      <Navbar />
+      <div style={{ display: "flex" }}>
+        <LeftBar />
+        <div style={{ flex: 6 }}>
+          <Outlet />
+        </div>
+        <RightBar />
+      </div>
+    </div>
+  )
+}
